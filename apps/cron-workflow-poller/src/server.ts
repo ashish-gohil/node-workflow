55377947d2ffc64097930ea31497a8d31bd7f46d
Ashish Gohil <ashishgohil148@gmail.com>
2026-04-23 10:27:40 +0530

    cron wworkflow poller done

--- apps/cron-workflow-poller/src/server.ts
+++ apps/cron-workflow-poller/src/server.ts
@@ -1,15 +1,41 @@
 import { SQSClient, SendMessageBatchCommand, } from "@aws-sdk/client-sqs";
 import { connectMongo, WorkflowModel } from "@repo/db";
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 
 const sqs = new SQSClient({
     region: "ap-south-1",
 });
 
-const QUEUE_URL = process.env.QUEUE_URL!;
-
+// find workflows which should run now based on cron/nextRunAt and push it to queue
 export const handler = async () => {
     console.log("Polling workflows...");
     connectMongo();
+
+    const ssm = new SSMClient({});
+    const ssmParamName = process.env.QUEUE_URL_PATH!;
+
+    if (!ssmParamName) {
+        throw new Error(
+            "QUEUE_URL is not defined in environment variables"
+        );
+    }
+
+    console.log("Fetching QUEUE URL from AWS SSM:", ssmParamName);
+
+    const ssmRes = await ssm.send(
+        new GetParameterCommand({
+            Name: ssmParamName,
+            WithDecryption: true,
+        })
+    );
+
+    const queueUrl = ssmRes.Parameter?.Value;
+    if (!queueUrl) {
+        throw new Error(`SSM parameter "${ssmParamName}" has no value`);
+    }
+
+
+    const QUEUE_URL = queueUrl;
 
     // un-lock workflows which are frizzed/locked before 5 mins(might bee lambda crashees and it make worrkflows only locked and not put in queue)
     const LOCK_TIMEOUT = 5 * 60 * 1000; // 5 min
@@ -38,9 +64,17 @@ export const handler = async () => {
         status: "READY",
         nextRunAt: { $lte: now },
     }).limit(100);
+    console.log("----active workflows-----")
+    console.log(activeWorkflows)
+    console.log("----active workflows-----")
+
 
     // first lock the workflows and then put in queue
     for (const wf of activeWorkflows) {
+        console.log("wf in for loop");
+        console.log(wf)
+        console.log("wf in for loop");
+
         const lockedWf = await WorkflowModel.findOneAndUpdate(
             {
                 workflowId: wf.workflowId,
@@ -59,9 +93,14 @@ export const handler = async () => {
         );
 
         if (lockedWf) {
+            console.log("-----updated locked wf-------")
             lockedWfs.push(lockedWf);
+            console.log("-----updated locked wf-------")
         }
     }
+    console.log("--------locked workflows--------")
+    console.log(lockedWfs)
+    console.log("--------locked workflows--------")
 
     const wfBatch = lockedWfs.map((wf) => ({
         Id: wf._id.toString(),
@@ -72,15 +111,16 @@ export const handler = async () => {
 
     })
     )
-    const command = new SendMessageBatchCommand({
-        QueueUrl: QUEUE_URL,
-        Entries: wfBatch,
+    if (wfBatch.length > 0) {
+        const command = new SendMessageBatchCommand({
+            QueueUrl: QUEUE_URL,
+            Entries: wfBatch,
 
-    });
-    const response = await sqs.send(command);
-    console.log("response from sqs")
-    console.log(response)
-
+        });
+        const response = await sqs.send(command);
+        console.log("response from sqs")
+        console.log(response)
+    }
 
     return { success: true };
 
